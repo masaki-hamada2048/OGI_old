7dbe727e9deb20eafcfb8ec5426f5ad94e613eca
masaki-hamada <32924891+masaki-hamada@users.noreply.github.com>
2017-12-19 17:36:17 +0900

    Update ogi.R

--- R/ogi.R
+++ R/ogi.R
@@ -1,16 +1,20 @@
 #' Weight Vectors of the Bi-unit Canonical Form
 #'
 #' \code{cov2weight(S)} returns the numeric vector in which the diagonal
-#'     elements of the matrix \eqn{D} are arranged, where \eqn{DSD} is
-#'     the bi-unit canonical form of \eqn{S}.
+#' elements of the matrix \eqn{D} are arranged, where \eqn{DSD} is the bi-unit
+#' canonical form of \eqn{S}.
 #'
 #' @param S Covariance matrix, especially it is positive semi-definite.
 #' @param Dvec Numeric vector of initial values of iteration.
-#' @param nu Numeric vector of subjective importance. It determines the importance of each of the variates.
-#' @param tol Numeric number of tolerance. If the minimum eigenvalue of \code{S} is less than \code{tol},
-#'     \code{S} is considered not to be positive definite.
-#' @param force Logical: If force=FALSE, \code{S} should be strictly positive definite. Default: FALSE.
-#' @return Numeric vector of diagonal elements of \eqn{D}, which appears in the bi-unit canonical form \eqn{DSD} of \eqn{S}.
+#' @param nu Numeric vector of subjective importance. It determines the
+#'   importance of each of the variates.
+#' @param tol Numeric number of tolerance. If the minimum eigenvalue of \code{S}
+#'   is less than \code{tol}, \code{S} is considered not to be positive
+#'   definite.
+#' @param force Logical: if force=FALSE, \code{S} should be strictly positive
+#'   definite. Default: FALSE.
+#' @return Numeric vector of diagonal elements of \eqn{D}, which appears in the
+#'   bi-unit canonical form \eqn{DSD} of \eqn{S}.
 #' @examples
 #' S = matrix(0, 5, 5)
 #' S[1,1] = 1
@@ -23,22 +27,6 @@
 #' }
 #' weight=cov2weight(S)
 #' weight
-#'
-#'
-#' p = stats::pbinom(0:(10-1), 10, 0.5) # binomial
-#' S = matrix(0, 10, 10)
-#' for(i in 1:10){
-#'   for(j in 1:10){
-#'     k = min(i,j)
-#'     S[i,j] = p[k] - p[i] * p[j]
-#'   }
-#' }
-#' methods::show(S)
-#' methods::show(eigen(S))
-#' d = cov2weight(S)
-#' e = sqrt(diag(S))
-#' graphics::plot(c(0, cumsum(e)), (c(0, cumsum(d)) - sum(d)/2)/sqrt(10), ylim=c(-3,3))
-#' d
 #' @export
 cov2weight = function(S, Dvec = rep(1, nrow(S)), nu = rep(1, nrow(S)), tol = 1e-06, force = FALSE){
   mineig = min(eigen(S)$val)
@@ -72,11 +60,15 @@ cov2weight = function(S, Dvec = rep(1, nrow(S)), nu = rep(1, nrow(S)), tol = 1e-
 
 #' Bi-unit Canonical Form
 #'
-#' \code{cov2biu(S)} returns the bi-unit canonical form of \eqn{S}.
+#' \code{cov2biu(S)} returns the bi-unit canonical form of \code{S}.
 #'
 #' @param S Covariance matrix, especially it is positive semi-definite.
-#' @param detail Logical: if \code{detail=TRUE}, it returns the list of the bi-unit form and the weight vectors.
-#'               Default: FALSE.
+#' @param nu Numeric vector of subjective importance. It determines the
+#'   importance of each of the variates.
+#' @param force Logical: if force=FALSE, \code{S} should be strictly positive
+#'   definite. Default: FALSE.
+#' @param detail Logical: if detail=TRUE, it returns the list of the bi-unit
+#'   form and the weight vectors. Default: FALSE.
 #' @return Numeric matrix of the bi-unit canonical form \eqn{DSD} of \eqn{S}.
 #' @examples
 #' S = matrix(0, 5, 5)
@@ -91,8 +83,8 @@ cov2weight = function(S, Dvec = rep(1, nrow(S)), nu = rep(1, nrow(S)), tol = 1e-
 #' B=cov2biu(S)
 #' B
 #' @export
-cov2biu = function(S, detail=FALSE){ # covariance matrix to bi-unit matrix
-  w = cov2weight(S)
+cov2biu = function(S, nu = rep(1, nrow(S)), force = FALSE, detail=FALSE){ # covariance matrix to bi-unit matrix
+  w = cov2weight(S, nu=nu, force=force)
   B = diag(w) %*% S %*% diag(w)
   dimnames(B) = dimnames(S)
   if(detail) return(list(B=B, weight=w))
@@ -101,42 +93,49 @@ cov2biu = function(S, detail=FALSE){ # covariance matrix to bi-unit matrix
 
 #' Objective General Index
 #'
-#' \code{ogi(X)} returns the objective general index (OGI) of the covariance matrix \code{S} of \code{X}.
+#' \code{ogi(X)} returns the objective general index (OGI) of the covariance
+#' matrix \code{S} of \code{X}.
 #'
-#' Consider a data matrix of \eqn{n} individuals with \eqn{p} variates. The objective general index (OGI)
-#' is a general index that combines the \eqn{p} variates into a univariate index in order to rank the
-#' \eqn{n} individuals. The OGI is always positively correlated with each of the variates.
-#' For more details, see the references.
+#' Consider a data matrix of \eqn{n} individuals with \eqn{p} variates. The
+#' objective general index (OGI) is a general index that combines the \eqn{p}
+#' variates into a univariate index in order to rank the \eqn{n} individuals.
+#' The OGI is always positively correlated with each of the variates. For more
+#' details, see the references.
 #'
 #' @param X Numeric or ordered matrix.
-#' @param se Logical: if \code{se=TRUE}, it additionally computes \code{w.se} and \code{v.se} by bootstrap.
-#'     Default: FALSE.
-#' @param force Logical: If force=FALSE, \code{S} should be strictly positive definite. Default: FALSE.
-#' @param se.loop Iteration number in bootstrap for computation of standard error.
-#' @param nu Numeric vector of subjective importance. It determines the importance of each column of \code{X}.
-#' @param center Logical: if center=TRUE, \code{ogi(X)$Z} is centered. Default:TRUE.
-#' @param mar Logical: if mar=TRUE, \code{X} is supposed to be the ordered categorical data. Default:FALSE.
-
+#' @param se Logical: if se=TRUE, it additionally computes \code{w.se} and
+#'   \code{v.se} by bootstrap. Default: FALSE.
+#' @param force Logical: if force=FALSE, \code{S} should be strictly positive
+#'   definite. Default: FALSE.
+#' @param se.loop Iteration number in bootstrap for computation of standard
+#'   error.
+#' @param nu Numeric vector of subjective importance. It determines the
+#'   importance of each column of \code{X}.
+#' @param center Logical: if center=TRUE, \code{ogi(X)$Z} is centered.
+#'   Default:TRUE.
+#' @param mar Logical: if mar=TRUE, each of ordered categorical variates of
+#'   \code{X} (if exists) is marginally converted into a numeric vector in
+#'   advance by the univariate OGI quantification. If mar=FALSE, the
+#'   simultaneous OGI quantification is applied. Default:FALSE.
 #' @return
-#'   \item{value}{The objective general index (OGI).}
-#'   \item{X}{The input matrix \code{X}.}
-#'   \item{scaled}{The product of \code{Z \%*\% diag(weight)},
-#'     where \code{Z} and \code{weight} are as follows.}
-#'   \item{Z}{Numerical matrix converted from X. If center = TRUE, it is centered.}
-#'   \item{weight}{The output of \code{\link{cov2weight}(S, nu=nu, force=force)}. }
-#'   \item{rel.weight}{The product of \code{weight * sqrt(diag(S))},
-#'     where \code{S} is the covariance matrix of \code{X}.}
-#'   \item{biu}{The bi-unit canonical form of the covariance matrix of \code{X}.}
-#'   \item{idx}{Numeric vector. If \code{X} has ordered categorical variates,
-#'     \code{idx} has (number of levels) -1 number of indexes.}
-#'   \item{w.se}{If requested, \code{w.se} is numeric vector of the standard error of
-#'     \code{weight}, where \code{S} is the covariance matrix of \code{X}.
-#'     It is calculated by bootstrap.}
-#'   \item{v.se}{If requested, \code{v.se} is numeric vector of the standard error of
-#'     \code{ogi(S)$value}, where \code{S} is the covariance matrix of \code{X}.
-#'     It is calculated by bootstrap.}
-#' @references  Sei, T. (2016). An objective general index for multivariate ordered data,
-#'   Journal of Multivariate Analysis, 147, 247-264.
+#' \item{value}{The objective general index (OGI).}
+#' \item{X}{The input matrix \code{X}.}
+#' \item{scaled}{The product of \code{Z \%*\% diag(weight)}, where \code{Z} and
+#'   \code{weight} are as follows.}
+#' \item{Z}{Numerical matrix converted from \code{X}. If center = TRUE, it is centered.}
+#' \item{weight}{The output of \code{\link{cov2weight}(S, nu=nu, force=force)},
+#'   where \code{S} is the covariance matrix of \code{X}. }
+#' \item{rel.weight}{The product of \code{weight * sqrt(diag(S))}, where \code{S}
+#'   is the covariance matrix of \code{X}.}
+#' \item{biu}{The bi-unit canonical form of the covariance matrix of \code{X}.}
+#' \item{idx}{Numeric vector. If \code{X} has ordered categorical variates,
+#'   \code{idx} has (number of levels) -1 number of indexes.}
+#' \item{w.se}{If requested, \code{w.se} is numeric vector of the standard error
+#'   of \code{weight}. It is calculated by bootstrap.}
+#' \item{v.se}{If requested, \code{v.se} is numeric vector of the standard error
+#'   of \code{value}. It is calculated by bootstrap.}
+#' @references  Sei, T. (2016). An objective general index for multivariate
+#'   ordered data, Journal of Multivariate Analysis, 147, 247-264.
 #'   \url{http://www.sciencedirect.com/science/article/pii/S0047259X16000269}
 #' @examples
 #' CT = matrix(c(
@@ -158,24 +157,30 @@ cov2biu = function(S, detail=FALSE){ # covariance matrix to bi-unit matrix
 #' X[,1] = factor(X0[,1], ordered=TRUE)
 #' X[,2] = factor(X0[,2], ordered=TRUE)
 #' ogiX = ogi(X)
-#' graphics::par(pty="s", cex=1.7, mar=c(4.5,3,1,1))
-#' graphics::plot(ogiX$scaled, xlim=c(-3,3), ylim=c(-3,3), xlab="Geometry", ylab="Probability")
+#' par(pty="s", cex=1.7, mar=c(4.5,3,1,1))
+#' plot(ogiX$scaled, xlim=c(-3,3), ylim=c(-3,3), xlab="Geometry", ylab="Probability")
 #' for(t in 1:nrow(ogiX$scaled)){
 #'   xy = ogiX$scaled[t,]
 #'   g = rep(sum(xy)/2, 2)
-#'   graphics::segments(xy[1], xy[2], g[1], g[2], lty=2)
+#'   segments(xy[1], xy[2], g[1], g[2], lty=2)
 #' }
-#' graphics::arrows(-3, -3, 3, 3)
-#' graphics::text(2.5, 2, "OGI/2")
+#' arrows(-3, -3, 3, 3)
+#' text(2.5, 2, "OGI/2")
 #' ogiX
 #'
 #'
 #' f = ordered(1:10)
 #' f[sample(1:10, 20, replace=TRUE)]
 #' Y = ogi(f)$value
-#' graphics::plot((1:10)/(10+1), Y, type="b")
+#' plot((1:10)/(10+1), Y, type="b")
 #' xs = (1:1000)/1001
-#' graphics::points(xs, stats::qnorm(xs), type="l", col="red")
+#' points(xs, qnorm(xs), type="l", col="red")
+#'
+#'
+#' X = USJudgeRatings
+#' ogiX = ogi(X)
+#' nameX = ordered(names(X), names(X))
+#' plot(nameX, ogiX$weight, las=3, cex.axis=0.8, ylim=c(0,1.2), ylab="weight")
 #' @export
 ogi = function(X, se=FALSE, force=FALSE, se.loop=1000, nu=rep(1, ncol(X)), center=TRUE, mar=FALSE){ # The main function
   if(!is.data.frame(X)) X = as.data.frame(X)
@@ -219,7 +224,7 @@ ogi = function(X, se=FALSE, force=FALSE, se.loop=1000, nu=rep(1, ncol(X)), cente
       ogiB = ogi(ZB, se=FALSE, force=TRUE)
       wB[,i] = ogiB$weight
     }
-    w.se = apply(wB, 1, sd)
+    w.se = apply(wB, 1, stats::sd)
     v.se = sqrt(Z^2 %*% w.se^2)
     ymax = max(OGI + v.se); ymin = min(OGI - v.se)
     graphics::plot(rowSums(my.scale(Z)), OGI, ylim=c(ymin,ymax), xlab="scaled sum", ylab="OGI")
